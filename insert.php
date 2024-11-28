fbb58dd2b0d6af44c6f54dc917a1a51fc1e91da4
John-Ichi <140050918+John-Ichi@users.noreply.github.com>
2024-11-28 11:28:08 +0800

    Add files via upload

--- insert.php
+++ insert.php
@@ -35,24 +35,74 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Arimo:ital,wght@0,400..700;1,400..700&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
     <title>Insert</title>
+    <style>
+        body {
+            text-align: center;
+            font-family: "Arimo", serif;
+            background-image: linear-gradient(to bottom right, #23cd6b, #272d2d);
+        }
+        .container {
+            background-color: #272d2d;
+            border-radius: 5px;
+            margin-top: 15px;
+            max-width: 400px;
+            text-align: left;
+            color: white;
+        }
+        .btn {
+            color: gray;
+        }
+        a {
+            text-decoration: none;
+            color: gray;
+        }
+        input {
+            margin: 10px;
+        }
+        h1 {
+            margin-top: 10px;
+            color: white;
+            text-shadow: 2px 2px 4px #000000;
+        }
+    </style>
 </head>
 <body>
+    <div class="container-sm">
+        <h1><b>ADD USER</b></h1>
+    </div>
+    <div class="container">
     <form method="post">
-        <input type="text" placeholder="Name" name="name" required><br>
-        <input type="text" placeholder="Email" name="email" required><br>
-        Birthday <br>
-        <input type="date" name="birthday" required><br>
-        <input type="text" placeholder="Address" name="address" required><br>
+        <input type="text" placeholder="Name" name="name" required class="form-control-sm mt-3"><br>
+        <input type="text" placeholder="Email" name="email" required class="form-control-sm"><br>
+        Birthday: <br>
+        <input type="date" name="birthday" required class="form-control-sm"><br>
+        <input type="text" placeholder="Address" name="address" required class="form-control-sm" maxlength="100"><br>
         Gender: <br>
-        <input type="radio" name="gender" value="Male" required>Male <br>
-        <input type="radio" name="gender" value="Female" required>Female <br>
-        <input type="radio" name="gender" value="Other" required>Other <br>
-        <button type="submit" name="submit">SUBMIT</button><br>
-        <button type="reset">RESET</button><br>
-        <button>
-            <a href="webpage.php">RETURN</a>
-        </button>
+        <input type="radio" name="gender" value="Male" required class="form-check-input"> Male <br>
+        <input type="radio" name="gender" value="Female" required class="form-check-input"> Female <br>
+        <input type="radio" name="gender" value="Other" required class="form-check-input"> Other <br>
+        <div class="container text-center pb-3">
+        <div class="row">
+            <div class="col">
+                <button type="submit" name="submit" class="btn btn-sm btn-light">SUBMIT</button><br>
+            </div>
+            <div class="col">
+                <button type="reset" class="btn btn-sm btn-light">RESET</button><br>
+            </div>
+            <div class="col">
+                <button class="btn btn-sm btn-light">
+                    <a href="webpage.php">RETURN</a>
+                </button>
+            </div>
+        </div>
+        </div>
     </form>
+    </div>
 </body>
 </html>
--- webpage.php
+++ webpage.php
@@ -8,33 +8,54 @@ include 'connect.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Arimo:ital,wght@0,400..700;1,400..700&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+    
     <title>Lab Activity 4</title>
 
     <style>
+        body {
+            font-family: "Arimo", serif;
+            background-image: linear-gradient(to bottom right, #23cd6b, #272d2d);
+            text-align: center;
+        }
+        h1 {
+            color: white;
+            text-align: left;
+            text-shadow: 2px 2px 4px #000000;
+        }
+        a {
+            color: gray;
+            text-decoration: none;
+        }
+        
     </style>
 
 </head>
 <body>
 
-<h1>USER INFORMATION</h1>
-
-<div>
-    <button>
-        <a href="insert.php">ADD USER</a>
-    </button>
+<div class="container">
+    <h1>
+        <b>USER INFORMATION</b>
+    </h1>
 </div>
+
 <br>
 
-<table>
+<div class="container">
+<table class="table table-success table-hover table-bordered border-success">
 <thead>
-    <tr>
+    <tr class="table table-dark">
         <th>ID</th>
         <th>NAME</th>
         <th>EMAIL</th>
         <th>BIRTHDAY</th>
         <th>ADDRESS</th>
         <th>GENDER</th>
-        <th>EDIT</th>
+        <th></th>
     </tr>
 </thead>
 
@@ -64,7 +85,7 @@ if ($result) {
         <td>' . $address . '</td>
         <td>' . $gender . '</td>
         <td>
-            <button>
+            <button class="btn btn-sm btn-light">
                 <a href="update.php?updatebyid=' . $id . '">EDIT</a>
             </button>
         </td>
@@ -76,6 +97,14 @@ if ($result) {
             ?>
         </tbody>
     </table>
+</div>
+
+<div>
+    <button class="btn btn-sm btn-light">
+        <a href="insert.php">ADD USER</a>
+    </button>
+</div>
+
 </body>
 
 </html>
